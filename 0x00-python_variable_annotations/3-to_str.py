fea7bcea1402faebbbe25350048dc3cded028cc3
dunsinadesina <adesinajesudunsin@gmail.com>
2024-07-04 04:36:33 -0700

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""a function that takes a float n as argument and returns the string rep"""
+
+
+def to_str(n: float) -> str:
+    """returns a string representation of a float
+    Args:
+        n: float to convert to string
+    Returns:
+        a string representation of a float
+    """
+    return str(n)
